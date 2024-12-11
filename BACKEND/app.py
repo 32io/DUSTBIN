1ae2085685b5f6ac639eb03de5b879071161dc1a
BRAVINE AMANI <amanibravin469@gmail.com>
2024-12-11 21:18:04 +0300

    wow

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -81,69 +81,69 @@ def initiate_payment(amount, email, phone, provider, dustbin):
 
 
 # 1. User Signup
-@app.route("/signup", methods=["POST"])
-def signup():
-    data = request.json
-    existing_user = db.users.find_one({"email": data["email"]})
-    if existing_user:
-        return jsonify({"error": "User already exists"}), 409
+# @app.route("/signup", methods=["POST"])
+# def signup():
+#     data = request.json
+#     existing_user = db.users.find_one({"email": data["email"]})
+#     if existing_user:
+#         return jsonify({"error": "User already exists"}), 409
 
-    hashed_password = generate_password_hash(data["password"])
-    db.users.insert_one({"email": data["email"], "password": hashed_password})
-    return jsonify({"message": "User created successfully"}), 201
-
-
-# 2. User Login
-@app.route("/login", methods=["POST"])
-def login():
-    data = request.json
-    user = db.users.find_one({"email": data["email"]})
-    if user and check_password_hash(user["password"], data["password"]):
-        session["user_id"] = str(user["_id"])
-        return jsonify({"message": "Logged in successfully"}), 200
-    return jsonify({"error": "Invalid credentials"}), 401
+#     hashed_password = generate_password_hash(data["password"])
+#     db.users.insert_one({"email": data["email"], "password": hashed_password})
+#     return jsonify({"message": "User created successfully"}), 201
 
 
-# 3. User Logout
-@app.route("/logout", methods=["POST"])
-def logout():
-    session.pop("user_id", None)
-    return jsonify({"message": "Logged out successfully"}), 200
+# # 2. User Login
+# @app.route("/login", methods=["POST"])
+# def login():
+#     data = request.json
+#     user = db.users.find_one({"email": data["email"]})
+#     if user and check_password_hash(user["password"], data["password"]):
+#         session["user_id"] = str(user["_id"])
+#         return jsonify({"message": "Logged in successfully"}), 200
+#     return jsonify({"error": "Invalid credentials"}), 401
 
 
-# 4. Add Dustbin
-@app.route("/add_dustbin", methods=["POST"])
-def add_dustbin():
-    user_id = session.get("user_id")
-    if not user_id:
-        return jsonify({"error": "Unauthorized"}), 403
-
-    data = request.json
-    db.dustbins.update_one(
-        {"dustbin_id": data["dustbin_id"]},
-        {
-            "$set": {
-                "user_id": user_id,
-                "dustbin_id": data["dustbin_id"],
-                # "state": "empty",
-                "location": data.get("location"),
-            }
-        },
-        upsert=True,
-    )
-    return jsonify({"message": "Dustbin added"}), 201
+# # 3. User Logout
+# @app.route("/logout", methods=["POST"])
+# def logout():
+#     session.pop("user_id", None)
+#     return jsonify({"message": "Logged out successfully"}), 200
 
 
-# 5. List Dustbins
-@app.route("/dustbins", methods=["GET"])
-def list_dustbins():
-    user_id = session.get("user_id")
-    if not user_id:
-        return jsonify({"error": "Unauthorized"}), 403
+# # 4. Add Dustbin
+# @app.route("/add_dustbin", methods=["POST"])
+# def add_dustbin():
+#     user_id = session.get("user_id")
+#     if not user_id:
+#         return jsonify({"error": "Unauthorized"}), 403
 
-    dustbins = list(db.dustbins.find({"user_id": user_id}))
-    print(dustbins)
-    return Response(dumps(dustbins), mimetype="application/json")
+#     data = request.json
+#     db.dustbins.update_one(
+#         {"dustbin_id": data["dustbin_id"]},
+#         {
+#             "$set": {
+#                 "user_id": user_id,
+#                 "dustbin_id": data["dustbin_id"],
+#                 # "state": "empty",
+#                 "location": data.get("location"),
+#             }
+#         },
+#         upsert=True,
+#     )
+#     return jsonify({"message": "Dustbin added"}), 201
+
+
+# # 5. List Dustbins
+# @app.route("/dustbins", methods=["GET"])
+# def list_dustbins():
+#     user_id = session.get("user_id")
+#     if not user_id:
+#         return jsonify({"error": "Unauthorized"}), 403
+
+#     dustbins = list(db.dustbins.find({"user_id": user_id}))
+#     print(dustbins)
+#     return Response(dumps(dustbins), mimetype="application/json")
 
 
 @app.route("/woww", methods=["GET"])
@@ -151,110 +151,110 @@ def home():
     return Response("I AM WORKING ")
 
 
-@app.route("/payment_start", methods=["POST"])
-def payment_processing():
-    data = request.json
-    user_email = data.get("email")
-    dustbin_id = data.get("dustbin_id")
-    phone_number = data.get("phone")
+# @app.route("/payment_start", methods=["POST"])
+# def payment_processing():
+#     data = request.json
+#     user_email = data.get("email")
+#     dustbin_id = data.get("dustbin_id")
+#     phone_number = data.get("phone")
 
-    provider = "mpesa"  # Mobile money provider
+#     provider = "mpesa"  # Mobile money provider
 
-    payment_response = initiate_payment(
-        300, user_email, phone_number, provider, dustbin_id
-    )
-    user_id = db.dustbins.find_one({"dustbin_id": dustbin_id}).get("user_id")
-    print(payment_response)
-    _logger.info(payment_response)
-    if user_id:
-        if "error" not in payment_response:
-            data = payment_response["data"]
-            print(data)
+#     payment_response = initiate_payment(
+#         300, user_email, phone_number, provider, dustbin_id
+#     )
+#     user_id = db.dustbins.find_one({"dustbin_id": dustbin_id}).get("user_id")
+#     print(payment_response)
+#     _logger.info(payment_response)
+#     if user_id:
+#         if "error" not in payment_response:
+#             data = payment_response["data"]
+#             print(data)
 
-            if data.get("status") == "pay_offline":
-                reference = data["reference"]
-                db.dustbins.update_one(
-                    {"dustbin_id": dustbin_id}, {"$set": {"reference_pay": reference}}
-                )
-                # db.pending_payments.insert_one(
-                #     {"user_id": user_id, "dustbin_id": dustbin_id, "payment_url": pay_url}
-                # )""
-                # redis_client.publish(
-                #     user_id,
-                #     json.dumps(
-                #         {
-                #             "message": "Payment pending",
-                #             "payment_state": "Pending",
-                #             "display": data["display_text"],
-                #         }
-                #     ),
-                # )
-            # print(reference)
-            # if reference:
-            else:
-                return (
-                    jsonify(
-                        {
-                            "error": "Payment initiation failed",
-                            "details": payment_response,
-                        }
-                    ),
-                    500,
-                )
-        else:
-            print(payment_response)
-            return (
-                jsonify(
-                    {"error": "Payment initiation failed", "details": payment_response}
-                ),
-                500,
-            )
+#             if data.get("status") == "pay_offline":
+#                 reference = data["reference"]
+#                 db.dustbins.update_one(
+#                     {"dustbin_id": dustbin_id}, {"$set": {"reference_pay": reference}}
+#                 )
+#                 # db.pending_payments.insert_one(
+#                 #     {"user_id": user_id, "dustbin_id": dustbin_id, "payment_url": pay_url}
+#                 # )""
+#                 # redis_client.publish(
+#                 #     user_id,
+#                 #     json.dumps(
+#                 #         {
+#                 #             "message": "Payment pending",
+#                 #             "payment_state": "Pending",
+#                 #             "display": data["display_text"],
+#                 #         }
+#                 #     ),
+#                 # )
+#             # print(reference)
+#             # if reference:
+#             else:
+#                 return (
+#                     jsonify(
+#                         {
+#                             "error": "Payment initiation failed",
+#                             "details": payment_response,
+#                         }
+#                     ),
+#                     500,
+#                 )
+#         else:
+#             print(payment_response)
+#             return (
+#                 jsonify(
+#                     {"error": "Payment initiation failed", "details": payment_response}
+#                 ),
+#                 500,
+#             )
 
-    return jsonify({"message": "Payment suceesful"}), 200
-
-
-# 6. Update Dustbin State
-@app.route("/dustbin_state", methods=["POST"])
-def update_dustbin_state():
-    data = request.json
-    dustbin_id = data.get("dustbin_id")
-    state = data.get("state")
-
-    db.dustbins.update_one(
-        {"dustbin_id": dustbin_id},
-        {"$set": {"state": state}},
-    )
-
-    # Publish message to Redis channel
-    user_id = db.dustbins.find_one({"dustbin_id": dustbin_id})
-    print(user_id)
-    user_id=user_id.get("user_id")
-    if user_id:
-        pass
-
-        # redis_client.publish(
-        #     user_id,
-        #     json.dumps(
-        #         {
-        #             "message": "Dustbin state updated",
-        #             "dustbin_id": dustbin_id,
-        #             "state": state,
-        #         }
-        #     ),
-        # )
-
-    return jsonify({"message": "Dustbin state updated"}), 200
+#     return jsonify({"message": "Payment suceesful"}), 200
 
 
-@app.route("/register_dustbin", methods=["POST"])
-def register_dustbin():
-    data = request.json
-    db.dustbins.update_one(
-        {"dustbin_id": data["dustbin_id"]},
-        {"$set": {"state": data.get("state", "empty")}},
-        upsert=True,
-    )
-    return jsonify({"message": "Dustbin registered successfully"}), 200
+# # 6. Update Dustbin State
+# @app.route("/dustbin_state", methods=["POST"])
+# def update_dustbin_state():
+#     data = request.json
+#     dustbin_id = data.get("dustbin_id")
+#     state = data.get("state")
+
+#     db.dustbins.update_one(
+#         {"dustbin_id": dustbin_id},
+#         {"$set": {"state": state}},
+#     )
+
+#     # Publish message to Redis channel
+#     user_id = db.dustbins.find_one({"dustbin_id": dustbin_id})
+#     print(user_id)
+#     user_id=user_id.get("user_id")
+#     if user_id:
+#         pass
+
+#         # redis_client.publish(
+#         #     user_id,
+#         #     json.dumps(
+#         #         {
+#         #             "message": "Dustbin state updated",
+#         #             "dustbin_id": dustbin_id,
+#         #             "state": state,
+#         #         }
+#         #     ),
+#         # )
+
+#     return jsonify({"message": "Dustbin state updated"}), 200
+
+
+# @app.route("/register_dustbin", methods=["POST"])
+# def register_dustbin():
+#     data = request.json
+#     db.dustbins.update_one(
+#         {"dustbin_id": data["dustbin_id"]},
+#         {"$set": {"state": data.get("state", "empty")}},
+#         upsert=True,
+#     )
+#     return jsonify({"message": "Dustbin registered successfully"}), 200
 
 
 # 7. SSE Notification Stream
@@ -276,57 +276,57 @@ def register_dustbin():
 
 
 # Webhook route to handle Paystack events
-@app.route("/webhook", methods=["POST"])
-def payment_webhook():
-    # Step 1: Verify signature to ensure the request is from Paystack
-    signature = request.headers.get("x-paystack-signature")
-    if not signature:
-        return jsonify({"error": "No signature provided"}), 403
+# @app.route("/webhook", methods=["POST"])
+# def payment_webhook():
+#     # Step 1: Verify signature to ensure the request is from Paystack
+#     signature = request.headers.get("x-paystack-signature")
+#     if not signature:
+#         return jsonify({"error": "No signature provided"}), 403
 
-    # Calculate HMAC SHA512 signature using your Paystack secret key
-    payload = request.get_data()
+#     # Calculate HMAC SHA512 signature using your Paystack secret key
+#     payload = request.get_data()
 
-    computed_signature = hmac.new(
-        PAYSTACK_SECRET_KEY.encode("utf-8"), payload, hashlib.sha512
-    ).hexdigest()
+#     computed_signature = hmac.new(
+#         PAYSTACK_SECRET_KEY.encode("utf-8"), payload, hashlib.sha512
+#     ).hexdigest()
 
-    if computed_signature != signature:
-        return jsonify({"error": "Invalid signature"}), 403
+#     if computed_signature != signature:
+#         return jsonify({"error": "Invalid signature"}), 403
 
-    # Step 2: Process the webhook event
-    data = request.json
-    print(request.json)
-    event_type = data.get("event")
+#     # Step 2: Process the webhook event
+#     data = request.json
+#     print(request.json)
+#     event_type = data.get("event")
 
-    # Handle 'charge.success' events
-    if event_type == "charge.success":
-        # Extract relevant information from the payload
-        reference = data["data"].get("reference")
-        status = data["data"].get("status")
-        if status == "success":
-            # Find the pending payment in MongoDB using the reference ID
-            print("here", reference)
-            dustbin = db.dustbins.find_one({"reference_pay": reference})
-            if dustbin:
-                # Update Dustbin state to "paid"
-                db.dustbins.update_one(
-                    {"reference_pay": reference}, {"$set": {"reference_pay": "None"}}
-                )
-                # Remove the pending payment entry
-                # db.pending_payments.delete_one({"reference": reference})
+#     # Handle 'charge.success' events
+#     if event_type == "charge.success":
+#         # Extract relevant information from the payload
+#         reference = data["data"].get("reference")
+#         status = data["data"].get("status")
+#         if status == "success":
+#             # Find the pending payment in MongoDB using the reference ID
+#             print("here", reference)
+#             dustbin = db.dustbins.find_one({"reference_pay": reference})
+#             if dustbin:
+#                 # Update Dustbin state to "paid"
+#                 db.dustbins.update_one(
+#                     {"reference_pay": reference}, {"$set": {"reference_pay": "None"}}
+#                 )
+#                 # Remove the pending payment entry
+#                 # db.pending_payments.delete_one({"reference": reference})
 
-                # Notify the user of the successful payment using Redis Pub/Sub
-                # redis_client.publish(
-                #     dustbin["user_id"],
-                #     json.dumps(
-                #         {
-                #             "message": "Payment received. Trash will be picked up within 2 hours.",
-                #             "status": "success",
-                #         }
-                #     ),
-                # )
+#                 # Notify the user of the successful payment using Redis Pub/Sub
+#                 # redis_client.publish(
+#                 #     dustbin["user_id"],
+#                 #     json.dumps(
+#                 #         {
+#                 #             "message": "Payment received. Trash will be picked up within 2 hours.",
+#                 #             "status": "success",
+#                 #         }
+#                 #     ),
+#                 # )
 
-    return jsonify({"status": "ok"}), 200
+#     return jsonify({"status": "ok"}), 200
 
 
 if __name__ == "__main__":
